ee4681b1e41ef7eff92d3e9615f7df956316f0d9
Divinegift <francisdivinegift@gmail.com>
2025-03-13 22:03:11 -0700

    okay nav v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/header-component/header'
 import React from 'react'
 const page = () => {
   return (
-   <div className="app">
+   <div className="app bg-white min-h-[100vh]">
     <Header/>
    </div>
   )
--- src/components/header-component/header.tsx
+++ src/components/header-component/header.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import img  from '../../../public/drivelink-icon.png'
+import Image from 'next/image'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className=' header bg-[rgba(182,204,255,0.095)] ' >
+      <div className="header-content min-h-[10vh] max-w-[1200px] mx-[auto] flex justify-between items-center bg-[rgba(182,204,255,0.095)] ">
+        <div className="logo w-[135px] flex items-center justify-between ">
+          <div className="drivelink-img w-[42px] ">
+          <Image src={img} alt='Drivelink icon' />
+          </div>
+          <span className='text-[20px] text-[#2E396D] font-[500]'>Drivelink</span>
+        </div>
+        <nav>
+            <ul className='flex text-[16px] w-[325px] justify-between font[400]   ' >
+              <li className='cursor-pointer'>About us</li>
+              <li className='font-[400] cursor-pointer'>How it Works</li>
+              <li className='cursor-pointer'>Services</li>
+            </ul>
+        </nav>
+        <div className="nav-btn font-[400] flex gap-[16px]  ">
+          <button className='bg-white w-[120px] h-[44px] text-[#101f91] font-[400] p-[16px] flex justify-center items-center d-block  rounded-[8px] cursor-pointer '>Contact us</button>
+          <button className='bg-[#101f91] text-white w-[128px] h-[44px] font-[400] p-[16px] rounded-[8px] flex justify-center items-center d-block cursor-pointer '>Download</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
