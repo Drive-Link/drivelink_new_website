c3c66b1cceaef31e7b459cf841aee6adb5d60463
Divinegift <francisdivinegift@gmail.com>
2025-03-13 23:10:56 -0700

    okay

--- src/components/hero-section/hero-section.tsx
+++ src/components/hero-section/hero-section.tsx
@@ -5,8 +5,15 @@ const HeroSection = () => {
     <div className="hero-section  bg-[rgba(182,204,255,0.095)] ">
         <div className="hero-section-content max-w-[1200px] mx-[auto] bg-white min-h-[80vh]  ">
             <div className="box1  ">
-                <span className='font-[700] text-[64px] '>Find and Hire <br /> Verified Drivers  </span> <br />
-                <span className='font-[700] text-[64px] text-[#101f91] ' >Fast and Hassle Free</span>
+                <span className='font-[700] text-[64px] leading-[72px] '>Find and Hire <br /> Verified Drivers  </span> <br />
+                <span className='font-[700] text-[64px] text-[#101f91] leading-[72px] ' >Fast and Hassle Free</span>
+                <div>
+                    <p className='leading-[26px] font-[400] text-[#828589] font-[18px] ' >Seamless rides, professional drivers, your car – your way! Book a verified <br /> driver instantly or schedule ahead for a stress-free driving experience.</p>
+                </div>
+                <div className="btn-group flex ">
+          <button className='bg-[#101f91] text-white w-[128px] h-[44px] font-[400] p-[16px] rounded-[8px] flex justify-center items-center d-block cursor-pointer '>Get Started</button>
+                <button className='bg-white min-w-[120px] h-[44px] text-[#101f91] font-[400] p-[16px] flex justify-center items-center d-block  rounded-[8px] cursor-pointer  '>Download App</button>
+                </div>
             </div>
             <div className="box2">
                  
