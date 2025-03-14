974864571266ed4f2ab8211361c10536933355f2
Divinegift <francisdivinegift@gmail.com>
2025-03-13 23:13:18 -0700

    okay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import About from '@/components/about-section/about'
 import Header from '@/components/header-component/header'
 import HeroSection from '@/components/hero-section/hero-section'
 import React from 'react'
@@ -6,6 +7,7 @@ const page = () => {
    <div className="app bg-white min-h-[100vh]">
     <Header/>
     <HeroSection/>
+    <About/>
    </div>
   )
 }
