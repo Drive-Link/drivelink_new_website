7c4a0db2f664572f6722b71862713526cfd3377b
Divinegift <francisdivinegift@gmail.com>
2025-03-13 22:45:51 -0700

    okay enter hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/header-component/header'
+import HeroSection from '@/components/hero-section/hero-section'
 import React from 'react'
 const page = () => {
   return (
    <div className="app bg-white min-h-[100vh]">
     <Header/>
+    <HeroSection/>
    </div>
   )
 }
---
+++ src/components/hero-section/hero-section.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section  bg-[rgba(182,204,255,0.095)] ">
+        <div className="hero-section-content max-w-[1200px] mx-[auto] bg-white min-h-[80vh]  ">
+            <div className="box1  ">
+                <span className='font-[700] text-[64px] '>Find and Hire <br /> Verified Drivers  </span> <br />
+                <span className='font-[700] text-[64px] text-[#101f91] ' >Fast and Hassle Free</span>
+            </div>
+            <div className="box2">
+                 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
