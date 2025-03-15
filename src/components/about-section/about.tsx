702e7d6beb623beb73ff947cb7348c66104aef40
Divinegift <francisdivinegift@gmail.com>
2025-03-14 17:52:28 -0700

    okay

--- src/components/about-section/about.tsx
+++ src/components/about-section/about.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 const About = () => {
   return (
    <div className="about-section">
-    <div className="section-content p-[20px] min-h-[80vh] border sm:p-0 ">
-
+    <div className="section-content p-[20px] min-h-[80vh] border max-w-[1200px] mx-[auto] sm:p-0 ">
+      <div className="headerabout flex justify-center ">
+        <span className='text-center font-[700] text-[25px] d-block ' >How Our Driver App Works: <br /> <span className='' >Simple and Fast </span> </span>
+      </div>
     </div>
    </div>
   )
