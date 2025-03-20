6a3649add88e0428e2e678bacf2f2273a8466dbc
onwumelu chijikoke <superkingsely@gmail.com>
2025-03-20 02:11:07 -0700

    okay

--- src/components/about-section/about.tsx
+++ src/components/about-section/about.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
 import abouticon from '../../../public/about-icon.png'
+import box1img1 from '../../../public/Mockup-box-1.png'
+import box1img2 from '../../../public/Mockup -box1(2).png'
+import aboutlinebox1 from '../../../public/about-line-box1.png'
 
 const About = () => {
   return (
@@ -14,16 +17,36 @@ const About = () => {
       </div>
       <div className="about-boxes flex grid grid-cols-1 sm:grid-cols-2 gap-8 sm:mt-[30px] mt-[40px] ">
         {/* box1 */}
-        <div className="about-box1 border h-[455px] basis-[335px] flex-1 bg-[#F4F7FF] px-[16px]  ">
-          <div className="icon mt-[30px] ">
+        <div className="about-box1 border  max-w-[550px] flex-1 bg-[#F4F7FF] px-[16px] sm:px-[26px] sm:rounded-[7px] sm:overflow-hidden ">
+          {/* icon and h1 */}
+          <div className="flex  flex-col gap-[30px] mt-[30px] sm:flex-row sm:items-center sm:gap-[20px] ">
+
+          <div className="icon  ">
             <div className="icon-bg flex justify-center items-center w-[40px] h-[40px] bg-[#101f91] rounded-[100%] ">
               <Image  src={abouticon} alt='star-icon' />
             </div>
           </div>
+          <div className="h">
+          <span className='font-[700] text-[16px] sm:text-[30px] ' >Download and Create Account</span>
+          </div>
+          </div>
           {/* writen content */}
-          <div className="writen-content mt-[30px] ">
-            <span className='font-[700] text-[16px] ' >Download and Create Account</span>
-            <p className=' text-[12px] leading-[18px] font-[400] text-[#828589] mt-[16px] max-w-[300px]  ' >Download the app from any store. Create an account,   verify your identity, and register your car details to start  booking professional drivers.</p>
+          <div className="writen-content mt-[20px] ">
+            
+            <p className=' text-[12px] sm:text-[16] leading-[18px] sm:leading-[28px] font-[400] text-[#828589]  max-w-[300px] sm:max-w-[400px]  ' >Download the app from any store. Create an account,   verify your identity, and register your car details to start  booking professional drivers.</p>
+          </div>
+          {/* mock-ups */}
+          <div className="img-group mt-[40px] sm:mt-[40px] flex items-end justify-start sm:justify-center relative ">
+            <div className="img1 w-[150px] sm:w-[200px] z-[1] ">
+              <Image src={box1img1} alt='' />
+            </div>
+            <div className="img2 ml-[-50px] sm:w-[200px] w-[150px]  z-[1]">
+              <Image src={box1img2} alt='' />
+            </div>
+            {/* ab-el */}
+            <div className="img absolute top-[-15]  left-0  w-[110%]  z-[0] sm:w-[110%]  ">
+              <Image className='w-[100%] sm:ml-[-8px]  ml-[-50px] h-[100%] ' src={aboutlinebox1} alt='lines' />
+            </div>
           </div>
         </div>
         {/* box2 */}
