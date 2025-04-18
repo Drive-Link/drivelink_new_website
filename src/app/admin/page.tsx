4086d5c82937018d70d919fddebd034ddecf212f
onwumelu chijikoke <superkingsely@gmail.com>
2025-04-18 03:03:10 -0700

    okay

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,12 @@
-import { LoginForm } from "@/components/login/admin-login.tsx"
+import React from 'react'
 
-export default function Page() {
+const page = () => {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
+    <div>
+      admin
+      
     </div>
   )
 }
+
+export default page
