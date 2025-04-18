ee639f07c5fe05b0e0fc8d743ff7cc1c47f7281e
onwumelu chijikoke <superkingsely@gmail.com>
2025-04-18 00:11:56 -0700

    okay

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -2,8 +2,8 @@ import Adminlogin from "@/components/login/admin-login";
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gray-50 ">
+      <div className="w-full max-w-sm bg-gray-50  ">
         <Adminlogin/>
       </div>
     </div>
